d782eeba18c40a1335779234232ffabfe2d34888
sahil <sahilshityalkar05@gmail.com>
2024-05-29 14:35:44 +0530

    create a front page , complete the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -18,19 +21,29 @@ const Hero = () => {
           fill="blue" 
         />
         </div>
-        <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.02] flex items-center justify-center absolute top-0 left-0">
       {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        Backgrounds
-      </p>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
     </div>
-    <div className='flex justify-center'>
-      <div className='max-w-[89vw]'>
-        <h1 className='uppercase track'>
-          Dynamic Web Magic With Next.js 14
-        </h1>
-        
+    <div className='flex justify-center relative my-20 z-10'>
+      <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+        <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-90'>
+        Specialist in Building Robust Web Applications
+        </h2>
+        <TextGenerateEffect
+        className='text-center text-[40px] md:text-5xl lg:text-6xl'
+        words='Merging Creativity with Cutting-Edge New Technologies'
+         />
+         <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+          Hi, I&apos;m Sahil, a Software Engineer based in India.
+         </p>
+         <a href="#about">
+         <MagicButton
+         title='Get My Resume'
+         icon={<FaLocationArrow />}
+         position='right'
+         />
+         </a>
       </div>
     </div>
     </div>
