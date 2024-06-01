2eb28be85ee4001a22df369a755c6cbaf2e93dff
sahil <sahilshityalkar05@gmail.com>
2024-06-01 11:46:33 +0530

    add resume download funtionality

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
           fill="blue" 
         />
         </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.02] flex items-center justify-center absolute top-0 left-0">
+        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.05] bg-grid-black/[0.02] flex items-center justify-center absolute top-0 left-0">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
     </div>
@@ -37,7 +37,7 @@ const Hero = () => {
          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
           Hi, I&apos;m Sahil, a Software Engineer based in India.
          </p>
-         <a href="#about">
+         <a href="/sahil_shityalkar_resume.pdf" download>
          <MagicButton
          title='Get My Resume'
          icon={<FaLocationArrow />}
